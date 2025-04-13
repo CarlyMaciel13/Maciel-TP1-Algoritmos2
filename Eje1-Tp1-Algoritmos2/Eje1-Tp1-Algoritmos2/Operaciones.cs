5ab2e040e9ca1c1c6b1e4100d99adc2ad052f696
CarlyMaciel13 <carla.a.maciel97@gmail.com>
2025-04-13 20:22:31 -0300

    Add files via upload

---
+++ Eje1-Tp1-Algoritmos2/Eje1-Tp1-Algoritmos2/Operaciones.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eje1_Tp1_Algoritmos2
+{
+    internal class Operaciones
+    {
+        #region Atributos
+        private int[] numeros; // Vector para almacenar los 10 números
+        private int mayor;     // Variable para guardar el número mayor
+        private int repeticiones; // Cantidad de veces que se repite el número mayor
+        #endregion
+
+        #region Constructor sin parámetros
+        public Operaciones()
+        {
+            Numeros = new int[10]; // Se inicializa el vector con tamaño 10
+            Mayor = int.MinValue;  // Inicia "mayor" con el valor más bajo posible para comparr luego
+            Repeticiones = 0;      // Inicializa el contador en 0
+        }
+        #endregion
+        #region Propiedades
+        public int[] Numeros { get => numeros; set => numeros = value; }
+        public int Mayor { get => mayor; set => mayor = value; }
+        public int Repeticiones { get => repeticiones; set => repeticiones = value; }
+        #endregion
+
+        #region Metodos
+        // Método para cargar los 10 números
+        public void CargarNumeros()
+        {
+            for (int i = 0; i < 10; i++) // Bucle que se repite 10 veces
+            {
+                Console.Write($"Ingrese el número {i + 1}: "); // Solicita número al usuario
+                Numeros[i] = int.Parse(Console.ReadLine());    // Guarda el número en el vector
+            }
+        }
+
+        // Método para calcular el número mayor
+        public void CalcularMayor()
+        {
+            // Recorre el vector para encontrar el número mayor
+            for (int i = 0; i < Numeros.Length; i++)
+            {
+                if (Numeros[i] > Mayor) // Si el número actual es mayor que el anterior mayor
+                {
+                    Mayor = Numeros[i]; // Actualiza el número mayor
+                }
+            }
+        }
+
+        // Método para contar cuántas veces se repite el número mayor
+        public void ContarRepeticiones()
+        {
+            Repeticiones = 0; // Reinicia el contador por si se llama varias veces
+
+            for (int i = 0; i < Numeros.Length; i++) // Recorre el vector
+            {
+                if (Numeros[i] == Mayor) // Si encuentra el número mayor
+                {
+                    Repeticiones++; // Incrementa el contador
+                }
+            }
+        }
+
+        // Método para mostrar los resultados
+        public void MostrarResultados()
+        {
+            Console.WriteLine($"\nEl número mayor es: {Mayor}"); // Muestra el número mayor(poner el $"" hace que se muestre el valor de la variable en medio de las {})
+            Console.WriteLine($"Se repitió {Repeticiones} veces."); // Muestra cuántas veces se repitió
+        }
+        #endregion
+    }
+}
---
+++ Eje1-Tp1-Algoritmos2/Eje1-Tp1-Algoritmos2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eje1_Tp1_Algoritmos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones op = new Operaciones(); // Se crea un objeto de la clase Operaciones
+
+            op.CargarNumeros();       // Carga los números
+            op.CalcularMayor();       // Calcula el número mayor
+            op.ContarRepeticiones();  // Cuenta cuántas veces se repite
+            op.MostrarResultados();   // Muestra el resultado final
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey(); // Pausa para que se pueda ver el resultado antes de que se cierre
+        }
+    }
+}
